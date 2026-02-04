067ccf565ecc7d0eb5da9e5c61c6206213da48f7
Rex <rexvancouverit@gmail.com>
2026-02-04 14:20:33 -0800

    Update Start.bat

--- Start.bat
+++ Start.bat
@@ -1,14 +1,11 @@
 @echo off
-:: %~dp0 代表脚本当前所在的路径
-:: cd /d 意思是“切换目录”
-cd /d "%~dp0server"
 
+cd /d "%~dp0server"
+taskkill /f /im deno.exe 2>nul
 echo ==========================================
 echo   YT-Smart-Assistant Server Starting...
 echo ==========================================
 
-:: 现在我们在 server 文件夹里了，直接运行 server.ts 即可
-deno run --allow-net --allow-run --allow-read --allow-write --allow-env server.ts
+.\deno run --allow-net --allow-run --allow-read --allow-write --allow-env server.ts
 
-:: 防止报错后窗口秒关，你看不到错误信息
 pause
